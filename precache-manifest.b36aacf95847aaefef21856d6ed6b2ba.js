15e84bd292c859cc5f1be4e3be9952edb30275ff
Priyanka <nandapriyanka13@gmail.com>
2022-11-12 16:37:02 +0530

    Updates

---
+++ precache-manifest.b36aacf95847aaefef21856d6ed6b2ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9ba1ede06c154dc79d1",
+    "url": "/Rich-Text-Editor/static/css/main.ed305edb.chunk.css"
+  },
+  {
+    "revision": "d9ba1ede06c154dc79d1",
+    "url": "/Rich-Text-Editor/static/js/main.d9ba1ede.chunk.js"
+  },
+  {
+    "revision": "c463bad5cb7f4d80cbc9",
+    "url": "/Rich-Text-Editor/static/js/1.c463bad5.chunk.js"
+  },
+  {
+    "revision": "604205f1a398dc6f7c12",
+    "url": "/Rich-Text-Editor/static/js/runtime~main.604205f1.js"
+  },
+  {
+    "revision": "06b29c72718e2e8f0bf9b759ca5fd62b",
+    "url": "/Rich-Text-Editor/static/media/logo.06b29c72.png"
+  },
+  {
+    "revision": "b85c753cc04f36dacd3b06969a7b8dbe",
+    "url": "/Rich-Text-Editor/index.html"
+  }
+];
